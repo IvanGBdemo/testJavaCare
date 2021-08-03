ac1d76214403c9ef01b0c47fd782e4a1d85ec2a2
IvanGBdemo <82132183+IvanGBdemo@users.noreply.github.com>
2021-08-03 22:31:41 +0300

    Update Main.java
    fixed by remark

--- src/main/java/HomeWork4/Main.java
+++ src/main/java/HomeWork4/Main.java
@@ -6,16 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Создал одномерный массив из 15 слов с повторением
+// Создал одномерный массив из 15 слов с повторением
         String[] arr = {"Watermelon", "Barberry", "Cowberry", "Elderberry", "Cherry", "Melon", "Honeysuckle",
                 "Wild strawberry", "Shadberry", "Viburnum", "Cranberry", "Cherry", "Rosehip", "Black currant", "Rowan"};
 
-//        HashMap<String, Integer> uniqueWords = new HashMap<>();
+// HashMap<String, Integer> uniqueWords = new HashMap<>();
 // Создал коллекцию типа HashMap, в которой после удаления останутся только уникальные элементы
         HashMap<String, Integer> listOfUniqueWords = new HashMap<>();
 
-//   Составил цикл for-else, который добавляет уникальные элементы в коллекцию из массива,
-//   и количество повторений слова в коллекции
+// Составил цикл for-else, который добавляет уникальные элементы в коллекцию из массива,
+// и количество повторений слова в коллекции
         for (int i = 0; i < arr.length; i++) {
             if (listOfUniqueWords.containsKey(arr[i])) {
                 listOfUniqueWords.put(arr[i], listOfUniqueWords.get(arr[i]) + 1);
